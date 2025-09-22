6fc71e3a5eafe61c3e14191e060f60b330018ff5
耗子 <haozi@loli.email>
2025-09-23 06:33:58 +0800

    feat: 优化大小

--- build_mysql.sh
+++ build_mysql.sh
@@ -71,7 +71,9 @@ rm -rf ${mysql_path}/bin/mysql_client_test*
 rm -rf ${mysql_path}/bin/mysqltest*
 rm -rf ${mysql_path}/bin/mysql_embedded
 
-# strip 二进制文件
+# 精简压缩
 strip -s ${mysql_path}/bin/*
+upx --best --lzma ${mysql_path}/bin/*
 
-echo "Installation successful"
+echo "Build successful"
+exit 0
